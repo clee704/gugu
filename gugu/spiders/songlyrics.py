6b794324737a79e086e3c7fe986855b9fb7dc6f4
Choongmin Lee <choongmin@me.com>
2014-08-11 04:10:13 +0900

    spiders: Add songlyrics

---
+++ gugu/spiders/songlyrics.py
@@ -0,0 +1,27 @@
+import re
+
+import scrapy
+from html2text import html2text
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy.contrib.spiders import CrawlSpider, Rule
+
+from ..items import Song
+
+class SonglyricsSpider(CrawlSpider):
+  name = 'songlyrics'
+  allowed_domains = ['songlyrics.com']
+  start_urls = ['http://www.songlyrics.com/']
+  rules = [
+    Rule(LinkExtractor(allow=('/[a-z0]/(\d+)?',))),
+    Rule(LinkExtractor(allow=('/[a-z-]+/[a-z-]+-lyrics/',),
+                       deny=('/news/',)), callback='parse_song'),
+    Rule(LinkExtractor(allow=('/[a-z-]+-lyrics/',))),
+  ]
+
+  def parse_song(self, response):
+    song = Song()
+    song['url'] = response.url
+    song['title'] = re.sub(r'(.*) Lyrics', r'\1', response.css('.current').xpath('text()').extract()[0])
+    song['artist'] = response.css('.pagetitle').xpath('.//a/text()').extract()[0]
+    song['lyrics'] = re.sub(r' *\n *', '\n', html2text(response.css('#songLyricsDiv').extract()[0]).strip())
+    yield song
