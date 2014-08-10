65633ae98ca46ffdc0e3b2b47343344bc9085ae6
Choongmin Lee <choongmin@me.com>
2014-08-11 03:15:49 +0900

    settings: Change UA string

--- gugu/settings.py
+++ gugu/settings.py
@@ -3,7 +3,7 @@ BOT_NAME = 'gugu'
 SPIDER_MODULES = ['gugu.spiders']
 NEWSPIDER_MODULE = 'gugu.spiders'
 
-USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.77.4 (KHTML, like Gecko) Version/7.0.5 Safari/537.77.4'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
 COOKIES_ENABLED = False
 DOWNLOAD_DELAY = 5
 
